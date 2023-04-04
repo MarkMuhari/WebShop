93a838c9a3e15790ea0d15c23679d51012eb68b8
MarkMuhari <markmuhari.eu@gmail.com>
2023-04-04 19:59:03 +0200

    Initial commit

---
+++ src/main/java/dev/markmuhari/webshop/backend/Application.java
@@ -0,0 +1,13 @@
+package dev.markmuhari.webshop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopApplication.class, args);
+	}
+
+}
