6a72fb0e5179181a9a9c5bcc43614e242429d5fe
MarkMuhari <markmuhari.eu@gmail.com>
2023-04-17 20:40:55 +0200

    Define the Router and set up Router Links

--- src/main/java/dev/markmuhari/webshop/frontend/angular-webshop/src/app/components/product-list/product-list.component.ts
+++ src/main/java/dev/markmuhari/webshop/frontend/angular-webshop/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -11,14 +12,35 @@ export class ProductListComponent implements OnInit{
 
 
   products!: Product[];
+  currentCategoryId!: number;
+
   
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) { }
 
   ngOnInit(){
-    this.listProducts();
+    this.route.paramMap.subscribe( () => {
+      this.listProducts();
+    });
+    
   }
 
   listProducts() {
+
+    // check if "id" parameter is avaible
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if(hasCategoryId) {
+      // get the "id" param string, convert string to number using the "+" symbol
+      this.currentCategoryId = Number(this.route.snapshot.paramMap.get('id'));
+    }
+    else {
+      // not category id avaible default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for given category id
+
     this.productService.getProductList().subscribe(
       data => {
         this.products = data;
